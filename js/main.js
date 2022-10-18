898f26441ec80438eb7fb6332ac5158d2dc56930
Samuel <samyamaya4@gmail.com>
2022-10-17 22:01:43 -0500

    AGREGANDO ANIMACIONES

---
+++ js/main.js
@@ -0,0 +1,16 @@
+console.log("JAVASCRIPT")
+
+// function articulos(){
+
+//     console.log(art)
+// }
+// articulos().
+function elementos(select){
+  if(select == 1){
+    return computadoras();
+  }
+}
+function computadoras(){
+  const computadoras = document.querySelector('#articulos-filtrados');
+  console.log(computadoras)
+}
